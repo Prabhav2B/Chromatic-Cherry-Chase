5f3e624546aacc75fdfa7f3a5f2038339c8f60f5
Prabhav Bhatnagar <prabhav2b@gmail.com>
2021-07-07 00:39:26 +0300

    slope and movement adjustments

--- Assets/Scripts/Player/CharController.cs
+++ Assets/Scripts/Player/CharController.cs
@@ -5,6 +5,7 @@ using UnityEngine.InputSystem;
 using DG.Tweening;
 using System;
 using Cinemachine;
+using UnityEngine.Serialization;
 
 public class CharController : MonoBehaviour
 {
@@ -14,8 +15,10 @@ public class CharController : MonoBehaviour
     [SerializeField, Range(-50f, 50f)] private float maxGravityAcceleration = -9.8f;
     [SerializeField, Range(0f, 10f)] private float jumpHeight = 5.0f;
     [SerializeField, Range(0f, 100f)] private float dashVelocity = 10f;
-    [SerializeField, Range(0f, 1f)]private float discount = 0.95f;
+    [FormerlySerializedAs("discount")] [SerializeField, Range(0f, 1f)]private float dashVelocityDiscount = 0.95f;
     [SerializeField, Range(0, 5)] private int maxAirJumps = 1;
+    [SerializeField, Range(0, 5)] private int maxAirDash = 1;
+
 
     [SerializeField] private bool fastFallActive = false;
 
@@ -23,8 +26,9 @@ public class CharController : MonoBehaviour
     private Vector2 velocity;
     private Vector2 displacement;
     private Vector2 acceleration;
-    private float moveVal;
     private Vector2 dashDir;
+    private Vector2 contactNormal;
+    private float moveVal;
     private float desiredMovementVelocity;
     private float desiredGravityVelocity;
     private float jumpVelocity;
@@ -35,8 +39,9 @@ public class CharController : MonoBehaviour
     private bool onGround;
     private bool fastFall;
     private int jumpPhase;
+    private int dashPhase;
     private float minGroundDotProduct;
-    private float naturalUpwardVelocity;
+    //private float naturalUpwardVelocity;
     private CinemachineImpulseSource _impulseSource;
 
     public bool JumpMaxed { get; private set; }
@@ -111,17 +116,19 @@ public class CharController : MonoBehaviour
             {
                 jumpPhase++;
                 jumpVelocity = Mathf.Sqrt(-2f * maxGravityAcceleration * jumpHeight);
+                //float alignedVelocity = Vector2.Dot(velocity.normalized, contactNormal); // not completely sure about this
 
-                if (velocity.y > 0f)
+                
+                if (!Mathf.Approximately(contactNormal.y, 1f) && velocity.y > 0) //check if on a upward slope
                 {
-                    jumpVelocity = Mathf.Max(jumpVelocity - velocity.y, 0f);
+                    velocity.y += ((jumpVelocity)) ;
                 }
-                else if (velocity.y < -0.001f)
+                else
                 {
-                    jumpVelocity = jumpVelocity - velocity.y;
+                    velocity.y = (jumpVelocity+ 1.5f) ;
                 }
 
-                velocity.y += jumpVelocity + 1.5f;
+                
             }
 
             if (jumpPhase >= maxAirJumps)
@@ -139,12 +146,7 @@ public class CharController : MonoBehaviour
             fastFall = false;
         }
 
-        float acceleration = onGround ? maxAcceleration : maxAirAcceleration;
-
-        var maxChangeSpeed = acceleration * Time.fixedDeltaTime;
-        if (Mathf.Approximately(desiredMovementVelocity, 0f) && !onGround)
-            maxChangeSpeed = maxAcceleration * Time.fixedDeltaTime;
-        velocity.x = Mathf.MoveTowards(velocity.x, desiredMovementVelocity, maxChangeSpeed);
+        AdjustVelocity();
 
         float maxGravityChange;
 
@@ -159,7 +161,7 @@ public class CharController : MonoBehaviour
 
         velocity.y = Mathf.MoveTowards(velocity.y, desiredGravityVelocity, maxGravityChange);
 
-        if (desiredDash)
+        if (dashPhase < maxAirDash && desiredDash)
         {
             
             //Might need to separate concerns 
@@ -169,6 +171,7 @@ public class CharController : MonoBehaviour
 
             if (!Mathf.Approximately(dashDir.sqrMagnitude, 0f))
             {
+                dashPhase++;
                 velocity.x = dashVelocity * dir.x;
                 velocity.y = dashVelocity * dir.y;
                 StopAllCoroutines();
@@ -182,16 +185,8 @@ public class CharController : MonoBehaviour
         onGround = false;
     }
 
-    IEnumerator DashWait(Vector2 velocity)
+    private IEnumerator DashWait(Vector2 velocity)
     {
-        //     
-        //     DOVirtual.Float(14, 0, .8f, RigidbodyDrag);
-        //
-        //     dashParticle.Play();
-        //     rb.gravityScale = 0;
-        //     GetComponent<BetterJumping>().enabled = false;
-        //     
-        //
         isDashing = true;
         rb.velocity = Vector2.zero;
         yield return new WaitForSeconds(.05f);
@@ -206,7 +201,7 @@ public class CharController : MonoBehaviour
                 break;
             yield return new WaitForFixedUpdate();
             timeElapsed += Time.fixedDeltaTime;
-            rb.velocity = dashVel * discount;
+            rb.velocity = dashVel * dashVelocityDiscount;
             dashVel = rb.velocity;
         }
 
@@ -217,13 +212,18 @@ public class CharController : MonoBehaviour
     {
         velocity = rb.velocity;
 
-        if (!onGround) return;
+        if (!onGround)
+        {
+            contactNormal = Vector2.up;
+            return;
+        }
         jumpPhase = 0;
-        naturalUpwardVelocity = 0f;
+        dashPhase = 0;
+        //naturalUpwardVelocity = 0f;
         JumpMaxed = false;
     }
 
-    void OnValidate()
+    private void OnValidate()
     {
         minGroundDotProduct = Mathf.Cos(maxGroundAngle * Mathf.Deg2Rad);
     }
@@ -243,7 +243,44 @@ public class CharController : MonoBehaviour
         for (int i = 0; i < collision.contactCount; i++)
         {
             Vector2 normal = collision.contacts[i].normal;
-            onGround |= normal.y >= minGroundDotProduct;
+
+
+            if (!(normal.y >= minGroundDotProduct)) continue;
+            onGround = true;
+            contactNormal = normal;
         }
+    }
+    
+    Vector2 ProjectOnContactPlane (Vector2 vector) {
+        return vector - contactNormal * Vector2.Dot(vector, contactNormal);
+    }
+
+    void AdjustVelocity()
+    {
+        Vector2 xAxis = ProjectOnContactPlane(Vector2.right).normalized;
+
+        float currentX = Vector2.Dot(velocity, xAxis);
+
+
+        float acceleration = onGround ? maxAcceleration : maxAirAcceleration;
+        float maxSpeedChange = acceleration * Time.fixedDeltaTime;
+
+        if (Mathf.Approximately(desiredMovementVelocity, 0f) &&
+            !onGround) //come to stop fast when control is released mid air
+            maxSpeedChange = maxAcceleration * Time.fixedDeltaTime;
+
+        float newX = Mathf.MoveTowards(currentX, desiredMovementVelocity, maxSpeedChange);
+
+        velocity += xAxis * (newX - currentX);
+
+        if (velocity.y < -0.1 && onGround) // little hack to adjust speed on slopes
+        {
+            velocity += velocity.normalized * 1.2f;
+        }
+        else if (velocity.y > 0.1 && onGround)
+        {
+            velocity -= velocity.normalized * 0.8f;
+        }
+
     }
 }
