7126b31afc78b84983ed0bb34c498dc5c63a6e94
Prabhav Bhatnagar <prabhav2b@gmail.com>
2022-02-21 14:08:11 +0200

    UI updates and polish

--- Assets/Scripts/UI/ControlSchemeChangeBroadcaster.cs
+++ Assets/Scripts/UI/ControlSchemeChangeBroadcaster.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 
 public class ControlSchemeChangeBroadcaster : MonoBehaviour
 {
@@ -15,13 +16,21 @@ public class ControlSchemeChangeBroadcaster : MonoBehaviour
     void Start()
     {
         _playerManager = FindObjectOfType<PlayerManager>();
+        
         _lastControlScheme = _playerManager.CurrentControlScheme;
         onControlSchemeChange?.Invoke(_lastControlScheme);
     }
-    
-    
+
+
     void Update()
     {
+        //this can be avoided somewhere somehow right?
+        if (_playerManager == null)
+        {
+            _playerManager = FindObjectOfType<PlayerManager>();
+            return;
+        }
+
         if(_lastControlScheme == _playerManager.CurrentControlScheme)
             return;
         
